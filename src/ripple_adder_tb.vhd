67d990897aeb3d84ba3048e562656d2d4eb8b6c8
SerenityMarek <c28serenity.marek@afacademy.af.edu>
2026-02-13 23:36:53 -0700

    ---

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -52,7 +52,7 @@ begin
        w_addends <= x"80"; w_Cin <= '0'; wait for 10 ns;
 	       assert (w_sum = x"8" and w_Cout = '0') report "bad with A3" severity failure;
        w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"F" and w_Cout = '0') report "bad with Cin" severity failure;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with Cin" severity failure;
 	   w_addends <= x"F0"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"0" and w_Cout = '1') report "bad with Cin and A input" severity failure;
        w_addends <= x"0F"; w_Cin <= '1'; wait for 10 ns;
