18f11d0d74cb2910ba18f663fd79318c375215c8
SerenityMarek <c28serenity.marek@afacademy.af.edu>
2026-02-13 23:26:43 -0700

    ICE3

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,6 +49,15 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
+       w_addends <= x"80"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"8" and w_Cout = '0') report "bad with A3" severity failure;
+       w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"F" and w_Cout = '0') report "bad with Cin" severity failure;
+	   w_addends <= x"F0"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "bad with Cin and A input" severity failure;
+       w_addends <= x"0F"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "bad with Cin and B input" severity failure;
+       
 	
 		wait; -- wait forever
 	end process;	
